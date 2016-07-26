58acf8163e43ddf1a15a66b50b2c5681406cfc11
yiyang186 <yiyang186@gmail.com>
2016-07-26 16:11:37 +0800

    增加了解压zip,rar文件的功能

--- copyrar.py
+++ copyrar.py
@@ -1,36 +1,63 @@
 from shutil import copy
 import os
 from optparse import OptionParser
+from decompress import *
+from planedata import *  # 该部分不上传
 
 class DirNode(object):
     def __init__(self, _path):
         self.data = _path
         if os.path.isdir(_path):
-            self.children = [os.path.join(_path, _file) for _file in os.listdir(_path)]
+            self.children = [os.path.join(_path, _file) for _file in 
+                             os.listdir(_path)]
         else:
             self.children = None
 
 class DirTree(object):
 
-    def __init__(self, source):
+    # temp是指定的临时文件夹，以'/'结尾，用于临时解压文件
+    def __init__(self, source, temp):
         self.root = self.build_dirtree(source)
+        self.tempdir = temp
 
     def build_dirtree(self, _path):
         node = DirNode(_path)
         if node.children:
-            node.children = [self.build_dirtree(child) for child in node.children]
+            node.children = [self.build_dirtree(child) for child in 
+                             node.children]
         return node
 
-    def copy(self, destination):
+    def extract_to(self, destination):
         self.dst = destination
-        self.copy_files(self.root)
+        self.traversal(self.root)
 
-    def copy_files(self, node):
+    def traversal(self, node):
         if not node.children and os.path.isfile(node.data):
-            copy(node.data, self.dst)
+            self.extract(node.data, self.dst)
         else:
             for child in node.children:
-                self.copy_files(child) 
+                self.traversal(child) 
+
+    def extract(data_path, dst):
+        filename, ext = os.path.splitext(data_path)
+        if ext == 'zip':
+            zfile = ZFile(filename)
+            zfile.extract_to(self.tempdir)
+        elif: ext == 'rar':
+            rfile = RFile(filename)
+            rfile.extract_to(self.tempdir)
+        elif: ext == 'dat':
+            # TODO
+        elif: ext in ['lod', 'DAT']:
+            # TODO
+        else:
+            # TODO
+
+    def my_unzip(self, filename):
+        z = zipfile.ZipFile(filename, 'r') 
+        for f in z.namelist(): 
+
+
 
 if __name__ == '__main__':
     op = OptionParser()
@@ -45,7 +72,7 @@ if __name__ == '__main__':
         os.makedirs(opts.dst)
 
     if os.path.isdir(opts.src) and os.path.isdir(opts.dst):
-        dt = DirTree(opts.src)
-        dt.copy(opts.dst)
+        dt = DirTree(extract_toopts.src)
+        dt.extract_to(opts.dst)
     else:
         print "wrong directoris!"
